3d8a1973fbf817de73c2ea3920298931b1cb0c37
Natan <netobelmiro1@gmail.com>
2025-11-04 11:31:09 -0300

    v1

--- demo/src/main/java/com/example/demo/loginapi/controller/AuthController.java
+++ demo/src/main/java/com/example/demo/loginapi/controller/AuthController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.loginapi.model.LoginRequest;
 import com.example.demo.loginapi.model.LoginResponse;
 import com.example.demo.loginapi.model.Usuario;
-import com.example.demo.loginapi.repository.UsuarioRepository;
+
 import com.example.demo.loginapi.service.AuthService;
 
-import org.apache.commons.logging.Log;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
