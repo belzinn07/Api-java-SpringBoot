d80603ad7a74c62c3c30e077c8c4476f1576dfb4
Belmiro Neto <netobelmiro1@gmail.com>
2025-11-10 20:59:25 -0300

    exclusao do gerenciamento toke

--- demo/src/main/java/com/example/demo/loginapi/service/TokenService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.loginapi.service;
-
-import com.example.demo.loginapi.model.Usuario;
-
-public interface TokenService {
-    String gerartoken(Usuario usuario);
-    String validarToken(String token);
-}
