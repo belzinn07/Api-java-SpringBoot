d882873b4a7d63ae5e4d784716c84d877f060e2a
Belmiro Neto <netobelmiro1@gmail.com>
2025-11-26 21:28:02 -0300

    Inicio manipulação jwt

--- demo/src/main/java/com/example/demo/loginapi/model/Usuario.java
+++ demo/src/main/java/com/example/demo/loginapi/model/Usuario.java
@@ -1,31 +1,55 @@
-package com.example.demo.loginapi.model;
-
 import jakarta.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
-    private String senha;
+    @Column(nullable=false)
     private String nome;
 
-    // Getters e Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    @Column(nullable=false, unique=true)
+    private String email;
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    @Column(nullable=false)
+    private String senha;
 
-    public String getSenha() { return senha; }
-    public void setSenha(String senha) { this.senha = senha; }
+    private String roles;
 
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
 
-    
+    }
+
+      public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+  
+    public String getRoles() {
+        return roles;
+    }
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
