4fcb048ab137224bf7aff95993e08118337a33ef
Belmiro Neto <netobelmiro1@gmail.com>
2025-11-27 15:15:42 -0300

    Implementação de filtro JWT e configuração de segurança

--- demo/src/main/java/com/example/demo/loginapi/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/loginapi/config/SecurityConfig.java
@@ -2,34 +2,61 @@ package com.example.demo.loginapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.demo.loginapi.security.JwtFilter;
+import com.example.demo.loginapi.security.JwtService;
+import com.example.demo.loginapi.security.UsuarioDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtService jwtService;
+    private final UsuarioDetailsService usuarioDetailsService;
+
+    public SecurityConfig(JwtService jwtService, UsuarioDetailsService usuarioDetailsService) {
+        this.jwtService = jwtService;
+        this.usuarioDetailsService = usuarioDetailsService;
+    }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        JwtFilter jwtFilter = new JwtFilter(jwtService, usuarioDetailsService);
         http
-           
-            .csrf().disable()
+          .csrf(csrf -> csrf.disable())
+          .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .authorizeHttpRequests(auth -> auth
+                  .requestMatchers("/api/login/**", "/h2-console").permitAll()
+                  .anyRequest().authenticated()
+          );
 
+          http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+
+          http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
           
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            )
+          return http.build();
 
-            
-            .formLogin().disable()
-            .httpBasic().disable()
+}
 
-           
-            .sessionManagement(sess -> sess
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+@Bean
+public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+}
 
-        return http.build();
-    }
+@Bean
+public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+
+}
 }
--- demo/src/main/java/com/example/demo/loginapi/security/JwtFilter.java
+++ demo/src/main/java/com/example/demo/loginapi/security/JwtFilter.java
@@ -12,8 +12,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtFilter extends OncePerRequestFilter {
-
+public class JwtFilter extends OncePerRequestFilter{
+   
     private final JwtService jwtService;
     private final UsuarioDetailsService userDetailsService;
 
@@ -21,12 +21,12 @@ public class JwtFilter extends OncePerRequestFilter {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
     }
-
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
            final String header = request.getHeader("Authorization");
-           final String prefix = "Bearer ";
+           final String prefix = "Bearer";
 
            if (header != null && header.startsWith(prefix)) {
                 String token = header.substring(prefix.length());
@@ -35,16 +35,15 @@ public class JwtFilter extends OncePerRequestFilter {
                     String email = jwtService.getSubject(token);
                     UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
-                    var auth = new UsernamePasswordAuthenticationToken(
+                    var auth = new UsernamePasswordAuthenticationToken( 
                             userDetails,
                             null,
                             userDetails.getAuthorities()
                     );
-
-            
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); 
+                    SecurityContextHolder.getContext().setAuthentication(auth); 
            }
-            
     }
-
+        filterChain.doFilter(request, response);
     
-}
+    }}
